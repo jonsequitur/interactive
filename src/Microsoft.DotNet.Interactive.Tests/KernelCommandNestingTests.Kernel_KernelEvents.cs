67d78bdb204d627814a152190a9df2726caffa14
Jon Sequeira <jonsequeira@gmail.com>
2025-02-25 12:40:05 -0800

    add test for issue

--- src/Microsoft.DotNet.Interactive.Tests/KernelCommandNestingTests.Kernel_KernelEvents.cs
+++ src/Microsoft.DotNet.Interactive.Tests/KernelCommandNestingTests.Kernel_KernelEvents.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.CSharp;
 using Microsoft.DotNet.Interactive.Events;
 using Microsoft.DotNet.Interactive.Tests.Utility;
+using Microsoft.FSharp.Collections;
 using Xunit;
 
 namespace Microsoft.DotNet.Interactive.Tests;
@@ -41,6 +44,35 @@ public partial class KernelCommandNestingTests
                         .Message
                         .Should()
                         .Be("(1,1): error CS0103: The name 'error' does not exist in the current context");
+        }
+
+        [Fact]
+        public async Task When_kernel_is_running_inside_another_kernel_its_KernelEvents_are_published()
+        {
+            var kernel = new CSharpKernel();
+
+            await kernel.SendAsync(new SubmitCode(
+                """
+                using Microsoft.DotNet.Interactive;
+                using Microsoft.DotNet.Interactive.Commands;
+                using Microsoft.DotNet.Interactive.Events;
+                using Microsoft.DotNet.Interactive.CSharp;
+                
+                var kernel = new CSharpKernel();
+                
+                var innerEvents = new List<KernelEvent>();
+                
+                kernel.KernelEvents.Subscribe(innerEvents.Add);
+                
+                await kernel.SendAsync(new SubmitCode("123"));
+                """));
+
+            kernel.TryGetValue("innerEvents", out List<KernelEvent> innerEvents);
+
+            innerEvents.Should().ContainSingle<ReturnValueProduced>()
+                       .Which
+                       .Value
+                       .Should().Be("123");
         }
     }
 }
